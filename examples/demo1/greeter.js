2869d5b7583c78fe58e7f314a606ef735b9c178c
namanpurii <puri2001naman@gmail.com>
2024-02-28 22:32:46 +0530

    interface example in ./examples/demo1

--- examples/demo1/greeter.js
+++ examples/demo1/greeter.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var hello_1 = require("./hello");
 function greeter(person) {
-    return "".concat(hello_1.default, ", ") + person;
+    return "".concat(hello_1.default, ", ") + person.firstName + " " + person.lastName;
 }
-var user = "User 1";
+var user = { firstName: "Naman", lastName: "Puri" }; //try changing this to some other type than what is emphasized by the type annotation above, and typescript shall throw an error
 document.body.textContent = greeter(user);
--- examples/demo1/greeter.ts
+++ examples/demo1/greeter.ts
@@ -1,9 +1,15 @@
 import hello from "./hello"
 
-function greeter(person: string) { // this is a type annotation of type 'string' to the parameter 'person'
-    return `${hello}, ` + person
+//interfaces in TypeScript
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-let user = "User 1" //try changing this to some other type than what is emphasized by the type annotation above, and typescript shall throw an error
+function greeter(person: Person) : string { // this is a type annotation of type 'Person' which is an interface to the parameter 'person'
+    return `${hello}, ` + person.firstName + " " + person.lastName
+}
+
+let user = {firstName: "Naman", lastName: "Puri"}//try changing this to some other type than what is emphasized by the type annotation above, and typescript shall throw an error
 
 document.body.textContent = greeter(user)
